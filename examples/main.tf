7f6d5f3fb022f137f1e76f7092c600d18be3e263
Yu-Lang Chu <yulang.chu@bitwala.com>
2022-02-04 23:54:40 +0100

    add provider

---
+++ examples/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    mambu = {
+      version = "0.2"
+      source  = "hashicorp.com/edu/mambu"
+    }
+  }
+}
+
+provider "mambu"{
+
+}
--- mambu/provider.go
+++ mambu/provider.go
@@ -1,13 +1,64 @@
 package mambu
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	client "github.com/Julian-Chu/MambuConfigurationAPI/configurationClient"
 )
+
+const KeyMambuBaseURL = "mambu_base_url"
+const KeyMambuApiKey = "mambu_apikey"
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap:   map[string]*schema.Resource{},
-		DataSourcesMap: map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{
+			KeyMambuBaseURL: &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("MAMBU_BASE_URL", nil),
+			},
+			KeyMambuApiKey: &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("MAMBU_API_KEY", nil),
+			},
+		},
+		ResourcesMap:         map[string]*schema.Resource{},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	mambuBaseURL := d.Get(KeyMambuBaseURL).(string)
+	mambuApiKey := d.Get(KeyMambuApiKey).(string)
+
+	var diags diag.Diagnostics
+
+	if mambuBaseURL != "" && mambuApiKey != "" {
+		c := client.NewClient(mambuBaseURL, mambuApiKey)
+		return c, diags
+	}
+
+	if mambuBaseURL == "" {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "mambu base url is required",
+			Detail:   "mambu base url is required",
+		})
+	}
+	if mambuApiKey == "" {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "mambu api key is required",
+			Detail:   "mambu api key is required",
+		})
+	}
+
+	return nil, diags
 }
